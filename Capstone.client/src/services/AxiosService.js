37553b6e44dab4ec4721149e3e18fa65e7801654
Q-Mick <quentinmick@protonmail.com>
2023-06-12 16:46:06 -0600

    added cheerio and fs

--- Capstone.client/src/services/AxiosService.js
+++ Capstone.client/src/services/AxiosService.js
@@ -1,10 +1,30 @@
 import Axios from 'axios'
 import { baseURL } from '../env'
 
+
 export const api = Axios.create({
   baseURL,
   timeout: 8000
 })
+
+export const componentApi = Axios.create({
+  baseURL: 'https://zylalabs.com/api/1126/the+components+finder+api/',
+  headers: { 
+    'Authorization': 'Bearer 1390|YGHOceSJJTXYkzcSh0tVxfB6hWhBcnurYS4zPhu1'
+    }
+})
+
+// case fans endpoint
+// GET https://zylalabs.com/api/1126/the+components+finder+api/989/list+of+case+fans
+
+// power supply endpoint
+// GET https://zylalabs.com/api/1126/the+components+finder+api/990/list+of+power+supplies
+     
+
+
+
+
+
 
 api.interceptors.request.use(config => config, handleAxiosError)
 api.interceptors.response.use(response => response, handleAxiosError)
