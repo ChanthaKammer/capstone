209259d747311e0389011502d6ad6ae6f0973d1f
Q-Mick <quentinmick@protonmail.com>
2023-06-12 16:46:25 -0600

    cheerio and fs

--- Capstone.client/src/pages/HomePage.vue
+++ Capstone.client/src/pages/HomePage.vue
@@ -6,13 +6,24 @@
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
         Vue 3 Starter
       </h1>
+      <button @click="getGraphicCardHTML()"> Get newegg graphic HTML</button>
     </div>
   </div>
 </template>
 
 <script>
+import { AppState } from '../AppState';
+import { Cheerio } from "cheerio";
+import { CheerioAPI } from "cheerio";
+import { CheerioOptions } from "cheerio";
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
 export default {
   setup() {
+function getGraphicCardHTML(){
+const url = 'https://www.newegg.com/p/pl?N=100007709&d=graphics+card&PageSize=96'
+}
+getGraphicCardHTML.
     return {}
   }
 }
