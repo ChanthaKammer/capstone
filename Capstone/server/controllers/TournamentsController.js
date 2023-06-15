35491fb1be57ca6d319804032c10200aa89de73b
Phillip <phill.c.egleston4@gmail.com>
2023-06-15 12:25:32 -0600

    createTournament

--- Capstone/server/controllers/TournamentsController.js
+++ Capstone/server/controllers/TournamentsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { tournamentsService } from "../services/TournamentsService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,12 +7,34 @@ export class TournamentsController extends BaseController{
         super('api/tournaments')
         this.router
             .get('', this.getAllTournaments)
+            .get('/:id', this.getTournamentById)
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.createTournament)
     }
 
+    async createTournament(req, res, next){
+        try {
+            const tournamentData = req.body
+            const tournament = await tournamentsService.createTournament(tournamentData)
+            return res.send(tournament)
+        } catch (error) {
+            next(error)
+        }
+    }
     async getAllTournaments(req, res, next){
         try {
             const tournaments = await tournamentsService.getAllTournaments()
             return res.send(tournaments)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getTournamentById(req, res, next){
+        try {
+            const tournamentId = req.body.id
+            const tournament = await tournamentsService.getTournamentById(tournamentId)
+            return res.send(tournament)
         } catch (error) {
             next(error)
         }
--- Capstone/server/services/TournamentsService.js
+++ Capstone/server/services/TournamentsService.js
@@ -1,9 +1,22 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 class TournamentsService{
+    async createTournament(tournamentData) {
+        const tournament = await dbContext.Tournaments.create(tournamentData)
+        await tournament.populate('creator particpantCount')
+        return tournament
+    }
+    async getTournamentById(tournamentId) {
+        const tournament = await dbContext.Tournaments.findById(tournamentId).populate('participantCount creator')
+        if(!tournament){
+            throw new BadRequest(`There is not tournament attached to ${tournamentId}`)
+        }
+        return tournament
+    }
     async getAllTournaments() {
-        const events = await dbContext.Tournaments.find().populate('participantCount creator')
-        return events
+        const tournaments = await dbContext.Tournaments.find().populate('participantCount creator')
+        return tournaments
     }
 
 }
