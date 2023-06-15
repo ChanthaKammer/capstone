d39991162b2dacb59cbf3e3ba1a24240eafa2056
Chantha Kammer <cadetsa219@gmail.com>
2023-06-15 15:30:56 -0600

    more template stuff

---
+++ Capstone.client/src/pages/Test.vue
@@ -0,0 +1,300 @@
+<template>
+   <section class="container-fluid">
+      <div class="row justify-content-center text-center">
+         <div class="col-3">
+            <h1>Play</h1>
+         </div>
+         <div class="col-3">
+            <h1>Host</h1>
+         </div>
+         <div class="col-3">
+            <h1>Compete</h1>
+         </div>
+      </div>
+   </section>
+   <section class="container-fluid">
+      <div class="row justify-content-center">
+         <div id="carouselExampleIndicators" class="carousel slide p-5 col-md-5">
+            <div class="carousel-indicators">
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" class="img-fluid" alt="...">
+               </div>
+               <div class="carousel-item">
+                  <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" class="img-fluid" alt="...">
+               </div>
+               <div class="carousel-item">
+                  <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" class="img-fluid" alt="...">
+               </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+            </button>
+         </div>
+      </div>
+   </section>
+   <section class="container-fluid pt-3">
+      <div class="row justify-content-around text-center">
+         <div class="col-3">
+            <div class="dropdown">
+               <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+               </button>
+               <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+               </ul>
+            </div>
+         </div>
+         <div class="col-3">
+            <div class="dropdown">
+               <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+               </button>
+               <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section class="container-fluid pt-3">
+      <div class="row">
+         <div class="col-md-3 pt-2 pb-2 px-2" style="">
+            <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" class="card-img-top rounded-top-3" alt="...">
+            <div class="card-body bg-dark rounded-bottom-3 p-2">
+               <h5 class="card-title">Card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="row text-center">
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 pt-2 pb-2 px-2" style="">
+            <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" class="card-img-top rounded-top-3" alt="...">
+            <div class="card-body bg-dark rounded-bottom-3 p-2">
+               <h5 class="card-title">Card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="row text-center">
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 pt-2 pb-2 px-2" style="">
+            <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" class="card-img-top rounded-top-3" alt="...">
+            <div class="card-body bg-dark rounded-bottom-3 p-2">
+               <h5 class="card-title">Card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="row text-center">
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 pt-2 pb-2 px-2" style="">
+            <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" class="card-img-top rounded-top-3" alt="...">
+            <div class="card-body bg-dark rounded-bottom-3 p-2">
+               <h5 class="card-title">Card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="row text-center">
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                  <div class="col">
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section class="container-fluid pt-3">
+      <div class="row justify-content-center py-2">
+         <div class="col-md-8">
+            <div class="row justify-content-between">
+               <div class="col-md-3">
+                  <h1>Overwatch</h1>
+                  <h1>Boise CodeWorks</h1>
+                  <h2>May 29 2000</h2>
+               </div>
+               <div class="col-md-4">
+                  <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" alt="" class="img-fluid">
+               </div>
+               <div class="col-md-3">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ab similique ullam culpa facilis odio!</p>
+                  <h1>Last Updated 2 Days Ago</h1>
+                  <div>
+                     <i class="mdi mdi-account-circle" style="font-size: 50px"></i>
+                     <i class="mdi mdi-account-circle" style="font-size: 50px"></i>
+                     <i class="mdi mdi-account-circle" style="font-size: 50px"></i>
+                     <i class="mdi mdi-account-circle" style="font-size: 50px"></i>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="row justify-content-center py-2">
+         <div class="col-md-8">
+            <div class="row justify-content-between">
+               <div class="col-md-3">
+                  <h1>Overwatch</h1>
+                  <h1>Boise CodeWorks</h1>
+                  <h2>May 29 2000</h2>
+               </div>
+               <div class="col-md-4">
+                  <img src="https://th.bing.com/th/id/R.f645b78e51a192cb4e9caa4aac6c0875?rik=ZTglhfD9mmwNIA&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2fgod-of-war-image.jpg&ehk=D%2bfse8wLp7FcFA%2bqBBv%2bCP0fIUGXa8YSkuFKGmU83yk%3d&risl=&pid=ImgRaw&r=0" alt="" class="img-fluid">
+               </div>
+               <div class="col-md-3">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ab similique ullam culpa facilis odio!</p>
+                  <h1>Last Updated 2 Days Ago</h1>
+                  <i class="mdi mdi-account-circle" style="font-size: 40px"></i>
+                  <i class="mdi mdi-account-circle" style="font-size: 40px"></i>
+                  <i class="mdi mdi-account-circle" style="font-size: 40px"></i>
+                  <i class="mdi mdi-account-circle" style="font-size: 40px"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <h1 class="text-center">END HOME PAGE</h1>
+   <section class="container-fluid">
+      <div class="row justify-content-center mt-3">
+         <div class="col-md-6 card shadow">
+            <h3 class="mt-1 text-center">Create Group</h3>
+            <input class="form-control mb-3" type="text" placeholder="Group Name" aria-label="groupName">
+            <input class="form-control mb-3" type="text" id="groupAvatarImage" placeholder="Group Avatar Image">
+            <input class="form-control mb-3" type="text" id="groupAvatarImage" placeholder="Group Cover Image">
+            <div class="row mb-3">
+               <div class="col-6"><input class="form-control" type="text" id="groupCity" placeholder="City"></div>
+               <div class="col-6"><input class="form-control" type="text" id="groupState" placeholder="State"></div>
+            </div>
+            <textarea class="form-control mb-3" id="exampleFormControlTextarea1" rows="3" placeholder="Group Description"></textarea>
+            <select class="form-select mb-3" aria-label="Default select example">
+               <option selected>Group Age Rating</option>
+               <option value="Everyone">Everyone</option>
+               <option value="Teen">Teen</option>
+               <option value="Adult">Adult</option>
+            </select>
+         </div>
+      </div>
+   </section>
+   <section class="container-fluid">
+      <div class="row justify-content-center mt-3 mb-3">
+         <div class="col-md-6 card shadow">
+            <h3 class="mt-1 text-center">Create Tournament</h3>
+            <input class="form-control mb-3" type="text" placeholder="Tournament Name" aria-label="groupName">
+            <input class="form-control mb-3" type="text" id="groupAvatarImage" placeholder="Tournament Avatar Image">
+            <input class="form-control mb-3" type="text" id="groupAvatarImage" placeholder="Tournament Cover Image">
+            <div class="row mb-3">
+               <div class="col-6"><input class="form-control" type="text" id="tournamentCity" placeholder="City"></div>
+               <div class="col-6"><input class="form-control" type="text" id="tournamentState" placeholder="State"></div>
+            </div>
+            <textarea class="form-control mb-3" id="exampleFormControlTextarea1" rows="3" placeholder="Tournament Description"></textarea>
+            <select class="form-select mb-3" aria-label="Default select example">
+               <option selected>Group Age Rating</option>
+               <option value="Everyone">Everyone</option>
+               <option value="Teen">Teen</option>
+               <option value="Adult">Adult</option>
+            </select>
+            <input class="form-control mb-3" type="number" placeholder="Max Teams" aria-label="maxTeams" min="1">
+            <input class="form-control mb-3" type="text" id="tournamentMoney" placeholder="Tournament Money Prize">
+         </div>
+      </div>
+   </section>
+   <section class="container-fluid">
+      <div class="row justify-content-center mb-3">
+         <ul class="col-6 nav justify-content-center">
+            <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Category
+               </a>
+               <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">All</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+               </ul>
+               </li>
+               <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     Age Rating
+                  </a>
+                  <ul class="dropdown-menu">
+                     <li><a class="dropdown-item" href="#">Everyone</a></li>
+                     <li><a class="dropdown-item" href="#">Teen</a></li>
+                     <li><a class="dropdown-item" href="#">Adult</a></li>
+                  </ul>
+               </li>
+            <li class="nav-item">
+               <button class="nav-link" href="#">Participants <i class="mdi mdi-arrow-down"></i></button>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link">Status <i class="mdi mdi-arrow-up"></i></a>
+            </li>
+         </ul>
+      </div>
+   </section>
+   <section class="container-fluid bg-dark text-white">
+      <div class="row justify-content-around">
+         <div class="col-2">
+            <div class="row">
+               <h4>Location</h4>
+               <h4>Email</h4>
+               <h4>Phone</h4>
+            </div>
+         </div>
+         <div class="col-5 text-center">
+            <h1>Placeholder Title</h1>
+         </div>
+         <div class="col-2">
+            <div class="row text-end">
+               <h4>Terms and Conditions</h4>
+               <h4>Privacy Policy</h4>
+               <h4>Jobs</h4>
+            </div>
+         </div>
+      </div>
+   </section>
+</template>
+
+<script>
+   export default {
+      setup() {
+         return {
+
+         }
+      }
+   }
+</script>
+
+<style scoped lang="scss">
+* {
+   border: 1px solid green;
+}
+</style>
--- Capstone.client/src/router.js
+++ Capstone.client/src/router.js
@@ -21,6 +21,11 @@ const routes = [
     name: 'Account',
     component: loadPage('AccountPage'),
     beforeEnter: authGuard
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: loadPage('Test')
   }
 ]
 
