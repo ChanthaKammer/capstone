4583ddac269661ffe3d3ebf4e5f76ef065238d3a
Q-Mick <quentinmick@protonmail.com>
2023-06-16 12:11:30 -0600

    added active game

--- Capstone.client/src/models/Game.js
+++ Capstone.client/src/models/Game.js
@@ -1,23 +1,7 @@
-export class Tournament {
+export class Game {
   constructor(data) {
-    this.id = data.id
-    this.creator = data.creator
-    this.creatorId = data.creatorId
-    this.name = data.name
-    this.description = data.description
-    this.capacity = data.capacity
-    this.totalRounds = data.totalRounds
-    this.currentRound = data.currentRound
-    this.createdAt = data.createdAt
-    this.updatedAt = data.updatedAt
-    this.isCancelled = data.isCancelled
-    this.isFinished = data.isFinished
-    this.type = data.type
-    this.gameSlug = data.gameSlug
-    this.gameName = data.gameName
-    this.gameImg = data.gameImg
-    this.participantCount = data.participantCount
-
+    this.coverImg = data
+.
     
   }
 }
