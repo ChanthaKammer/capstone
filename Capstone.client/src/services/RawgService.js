bb3dae672f82eed7d1b0e5954dcb4f0e93d86e53
Q-Mick <quentinmick@protonmail.com>
2023-06-16 11:27:00 -0600

    search functioning

--- Capstone.client/src/services/RawgService.js
+++ Capstone.client/src/services/RawgService.js
@@ -1,15 +1,15 @@
 import { rawgApi } from "./AxiosService.js";
 import { logger } from "../utils/Logger.js"
 class RawgService {
-  async getGames() {
-    const res = await rawgApi.get(``)
-    logger.log('[RAWG DATA]', res.data)
-  }
-  // async searchGames() {
-  //   const res = await rawgApi.get(`/games?key=c10295c7f83d492e8e807604409`, {
-  //     params: {search: 'search=diablo'}
-  //   })
+  // async getGames() {
+  //   const res = await rawgApi.get(``)
   //   logger.log('[RAWG DATA]', res.data)
   // }
+  async searchGames(searchTerm) {
+    const res = await rawgApi.get(``, {
+      params: {search: `${searchTerm}`}
+    })
+    logger.log('[SEARCH RESULTS]', res.data)
+  }
 }
 export const rawgService = new RawgService();
