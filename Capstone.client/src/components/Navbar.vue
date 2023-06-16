245e6f5fee80fcbc11e29727efd248d015df7d0e
Chantha Kammer <cadetsa219@gmail.com>
2023-06-16 15:56:08 -0600

    navbar

--- Capstone.client/src/components/Navbar.vue
+++ Capstone.client/src/components/Navbar.vue
@@ -8,31 +8,6 @@
             <img alt="logo" src="src/assets/img/gamePursuitLogo.png" class="rounded-pill elevation-2 shadow-lg me-3" style="" height="65" /><b>Game Pursuit</b></div>
         </div>
       </router-link>
-      
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll offset-1" style="--bs-scroll-height: 100px;">
-          <li class="nav-item mx-2">
-            <router-link :to="{ name: 'About' }" class="btn lighten-30 selectable text-uppercase">
-              Builder
-            </router-link>
-          </li>
-          <li class="nav-item mx-2">
-            <a class="nav-link" href="#">Build Guides</a>
-          </li>
-          <li class="nav-item mx-2">
-            <a class="nav-link" href="#">Completed Builds</a>
-          </li>
-          <li class="nav-item mx-2 dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Browse Products
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
         <form @submit.prevent="searchGames()" class="d-flex" role="search" style="width: 40%;">
           <input class="form-control me-2" type="search" placeholder="Search Games..." aria-label="Search" v-model="search">
           <button class="btn btn-primary me-3" style="opacity: .7;" type="submit">Search</button>
