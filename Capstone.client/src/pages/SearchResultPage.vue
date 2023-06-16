0ecfd5e7055dff3a286d75259894c7bcf4ad7d41
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-06-16 15:33:03 -0600

    Modal not popping any more errors

--- Capstone.client/src/pages/SearchResultPage.vue
+++ Capstone.client/src/pages/SearchResultPage.vue
@@ -11,13 +11,12 @@
                         <h1> {{ game.name }} </h1>
                         <h1> {{ game.released }} </h1>
                         <h1> {{ game.genres }} </h1>
-                        <button class="btn btn-success float-end" data-bs-toggle="modal" data-bs-target="#tournamentModal">Create Tournament</button>
                      <div class="row justify-content-center">
                         <!-- Modal -->
                         
-                                     <Modal id="tournamentModal">
+                                 
                                        <NewTournamentForm />
-                                     </Modal>
+                           
                                   </div>
                                 </div>
                               </div>
@@ -35,44 +34,12 @@
                         <h1>Diablo 4</h1>
                         <h1>Released: 2017-04-04</h1>
                         <h1>Game Genre</h1>
-                        <button class="btn btn-success float-end" data-bs-toggle="modal" data-bs-target="#eventModal">Create Tournament</button>
-                        <div class="row justify-content-center">
-                           <!-- Modal -->
-                           <div class="modal fade" id="eventModal" tabindex="-1" aria-labelledby="eventModal" aria-hidden="true">
-                              <div class="modal-dialog">
-                              <div class="modal-content">
-                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="createEventLabel">Create an Event</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                 </div>
-                                 <div class="modal-body justify-content-center">
-                                    <form @submit.prevent="createEvent" class="col-12">
-                                       <input class="form-control mb-3" type="text" placeholder="Tournament Name" aria-label="groupName">
-                                       <input class="form-control mb-3" type="url" id="groupAvatarImage" placeholder="Tournament Avatar Image">
-                                       <input class="form-control mb-3" type="url" id="coverImg" placeholder="Tournament Cover Image" v-model="editable.coverImg">
-                                       <div class="row mb-3">
-                                          <div class="col-6"><input class="form-control" type="text" id="location" placeholder="City" v-model="editable.location"></div>
-                                          <div class="col-6"><input class="form-control" type="text" id="tournamentState" placeholder="State"></div>
-                                       </div>
-                                       <textarea class="form-control mb-3" id="exampleFormControlTextarea1" rows="3" placeholder="Tournament Description" v-model="editable.description"></textarea>
-                                       <select class="form-select mb-3" aria-label="Default select example">
-                                          <option selected>Group Age Rating</option>
-                                          <option value="Everyone">Everyone</option>
-                                          <option value="Teen">Teen</option>
-                                          <option value="Adult">Adult</option>
-                                       </select>
-                                       <input class="form-control mb-3" type="number" placeholder="Max Teams" aria-label="maxTeams" min="1">
-                                       <input class="form-control mb-3" type="text" id="tournamentMoney" placeholder="Tournament Money Prize">
-                                       <button class="btn btn-success text-end" type="submit" role="button">Create Event</button>
-                                    </form>
-                                 </div>
-                              </div>
-                              </div>
-                           </div>
-                        </div>
+                        
+                           
+                 
+                     </div>
                   </div>
                </div>
-            </div>
 
       </section>
 </template>
@@ -81,10 +48,11 @@
 import Pop from '../utils/Pop.js';
 import { AppState } from '../AppState.js';
 import { logger } from '../utils/Logger.js';
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { rawgService } from "../services/RawgService.js";
-import { computed } from "vue";
-// import { Modal } from "bootstrap";
+import { tournamentsService } from "../services/TournamentsService.js"
+import { computed, ref } from "vue";
+import { Modal } from "bootstrap";
 import NewTournamentForm from "../components/NewTournamentForm.vue"
 
    export default {
@@ -94,8 +62,9 @@ import NewTournamentForm from "../components/NewTournamentForm.vue"
       },
 
       setup() {
-
+         const editable = ref({});
          const route = useRoute();
+         const router = useRouter();
 
          async function getDetailsBySlug() {
             try {
@@ -109,6 +78,24 @@ import NewTournamentForm from "../components/NewTournamentForm.vue"
          }
 
          return {
+            editable,
+            async createTournament(){
+               try {
+                  const tournamentData = editable.value
+                  const newTournament = await tournamentsService.createTournament(tournamentData)
+                  Modal.getOrCreateInstance('#tournamentModal').hide()
+                  editable.value = {}
+                  router.push(
+                     { name: 
+                        'TournamentDetails', 
+                       params: {
+                         tournamentId: newTournament.id 
+                        }}
+                        )
+               } catch (error) {
+                  Pop.error(error)
+               }
+            },
             getDetailsBySlug,
             game: computed(() => AppState.games),
          }
