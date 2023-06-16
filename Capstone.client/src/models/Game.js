f8931390b73b49774136716a67cc49e538f6f92b
Chantha Kammer <cadetsa219@gmail.com>
2023-06-16 12:31:09 -0600

    Update Game.js

--- Capstone.client/src/models/Game.js
+++ Capstone.client/src/models/Game.js
@@ -3,6 +3,7 @@ export class Game{
       this.slug = data.slug
       this.name = data.name
       this.platform = data.platform
+      this.platforms = data.platforms
       this.images = data.short_screenshots
       this.backgroundImg = data.background_image
       this.genres = data.genres
