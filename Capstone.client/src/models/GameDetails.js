259c8240b971e7f82fcf120636d0c2c8c112e1cb
Chantha Kammer <cadetsa219@gmail.com>
2023-06-16 12:25:39 -0600

    Create GameDetails.js

---
+++ Capstone.client/src/models/GameDetails.js
@@ -0,0 +1,18 @@
+export class GameDetails{
+   constructor(data){
+      this.slug = data.slug
+      this.name = data.name
+      this.description = data.description
+      this.metacritic = data.metacritic
+      this.released = data.released
+      this.rating = data.rating
+      this.parent_platforms = data.parent_platforms
+      this.platforms = data.platforms
+      this.stores = data.stores
+      this.developers = data.developers
+      this.genres = data.genres
+      this.tags = data.tags
+      this.publishers = data.publishers
+      this.esrb_rating = data.esrb_rating
+   }
+}
