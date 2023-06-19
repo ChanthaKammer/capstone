3f7a36818cc576fd772e5ec74d5ff2cae8a1b1e2
Phillip <phill.c.egleston4@gmail.com>
2023-06-19 09:38:33 -0600

    rewardsStuff

--- Capstone/server/services/RewardsService.js
+++ Capstone/server/services/RewardsService.js
@@ -1,7 +1,11 @@
+import { dbContext } from "../db/DbContext.js"
 
 
 class RewardsService {
-    
+    async createReward(rewardData) {
+        const reward = await dbContext.Rewards.create(rewardData).
+    }
+
 }
 
 export const rewardsService = new RewardsService()
