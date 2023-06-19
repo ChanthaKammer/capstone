1ec349542d52cff7bf5eadc25b1305c75f26eb0d
Phillip <phill.c.egleston4@gmail.com>
2023-06-19 09:26:20 -0600

    rewardsStuff

---
+++ Capstone/server/controllers/RewardsController.js
@@ -0,0 +1,8 @@
+import BaseController from "../utils/BaseController.js";
+
+export class RewardsController extends BaseController{
+    constructor(){
+        super('api/rewards')
+            
+    }
+}
