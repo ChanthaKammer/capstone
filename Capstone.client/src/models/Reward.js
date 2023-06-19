6d3ee067d11569a67ade02fd9dc5e7a759f0741c
Phillip <phill.c.egleston4@gmail.com>
2023-06-19 16:13:22 -0600

    rewardstuff

---
+++ Capstone.client/src/models/Reward.js
@@ -0,0 +1,9 @@
+
+export class Reward{
+    constructor(data){
+        this.name = data.name
+        this.tournamentId = data.tournamentId
+        this.accountId = data.accountId
+        
+    }
+}
