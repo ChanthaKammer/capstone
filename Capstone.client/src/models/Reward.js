7fcc44210c1439428ec7e909472c947e423e18ef
Swpogue <swpogue02@gmail.com>
2023-06-20 12:24:58 -0600

    Reward id

--- Capstone.client/src/models/Reward.js
+++ Capstone.client/src/models/Reward.js
@@ -1,6 +1,7 @@
 
 export class Reward{
     constructor(data){
+        this.id = data.id
         this.name = data.name
         this.tournamentId = data.tournamentId
         this.accountId = data.accountId
