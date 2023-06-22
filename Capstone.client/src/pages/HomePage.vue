91cd2a945da7061f37245d5a94c29eb19d900c84
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-06-22 16:17:14 -0600

    carousel images selectable to active tournament

--- Capstone.client/src/pages/HomePage.vue
+++ Capstone.client/src/pages/HomePage.vue
@@ -42,82 +42,22 @@
       <div class="col-12 bg-carousel justify-content-center align-items-center shadow-lg elevation-5">
         <Carousel ref="homeCarousel" :itemsToShow="3.95" :wrapAround="true" :transition="500" class="">
           <Slide v-for="slide in carouselTournaments" :key="slide" class="">
-            <div class="carousel__item"><img :src="slide.coverImg" alt="" class="selectable" @click=""></div>
+            <router-link :to="{name: 'TournamentDetails', params: {tournamentId: slide.id}}">
+
+              <div class="carousel__item"><img :src="slide.coverImg" alt=""></div>
+            </router-link>
           </Slide>
         </Carousel>
       </div>
+    </section>
 
         <!-- <HomeCarousel/> -->
 
         <!-- #region -->
         <!-- NOTE BACKGROUND BORDER FOR CAROUSEL ----------------------------->
-        <!-- 
-
-          SECTION CAROUSEL FOR 'MOST POPULAR' TOURNAMENTS BY SELECTABLE COVER IMAGE ***START*** ----------------------------
-          <div class="bg-games">
-            <div id="carouselExampleIndicators" class="carousel slide">
-              <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                  aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                  aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                  aria-label="Slide 3"></button>
-              </div>
-              <div class="carousel-inner p-5">
-                <div class="carousel-item active">
-                  <img
-                    src="https://th.bing.com/th/id/R.4c1438a6790709e13fedec0919ab9262?rik=vuldbqZ%2fyCjFpA&pid=ImgRaw&r=0"
-                    class="d-block w-75 game-img rounded-3" style="position: relative; right: 12%; left: 12%;" alt="...">
-                  <div class="carousel-caption d-none d-md-block mb-5"
-                    style="font-weight: 550; text-shadow: 0 2px 3px black;">
-                    <h1 class="caption-text-2" style="filter: brightness(1.5)">Star Citizen</h1>
-                    <h5 class="caption-text-2" style="filter: brightness(1.5);"><b>#1 game for anyone looking to explore a
-                        creative universe built around cosmic destruction.</b></h5>
-                  </div>
-                </div>
-                <div class="carousel-item">
-                  <div v-if="tournaments">
-                    <div v-for="t in tournaments" :key="t.id">
-                      <img
-                        :src="t.coverImg"
-                        class="d-block w-75 game-img rounded-3" style="position: relative; right: 12%; left: 12%;" alt="...">
-                    </div>
-                  </div>
-                  <div class="carousel-caption d-none d-md-block mb-5"
-                    style="font-weight: 550; text-shadow: 0 2px 3px black;">
-                    <h1 class="caption-text-1" style="filter: brightness(1.5)">GOD OF WAR</h1>
-                    <h5 class="caption-text-1" style="filter: brightness(1.5);"><b>Join the battle with others that are
-                        slaying their way to the top of Olympus.</b></h5>
-                  </div>
-                </div>
-                <div class="carousel-item">
-                  <img src="https://cdn.mos.cms.futurecdn.net/Goqyz5GacSCWcBHT522gJ9-970-80.jpg.webp"
-                    class="d-block w-75 game-img rounded-3" style="position: relative; right: 12%; left: 12%;" alt="...">
-                  <div class="carousel-caption d-none d-md-block mb-5"
-                    style="font-weight: 550; text-shadow: 0 2px 3px black;">
-                    <h1 class="caption-text-3" style="filter: brightness(1.5)">Elden Ring</h1>
-                    <h5 class="caption-text-3" style="filter: brightness(1.5);"><b>Up for something extra challenging? See
-                        the kind of dedication brought to the table by checking out our Most OP monster slayers!</b></h5>
-                  </div>
-                </div>
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
-          </div>
-        </div> -->
+        
 <!-- #endregion -->
 
-    </section>
     <!-- SECTION CAROUSEL FOR 'MOST POPULAR' TOURNAMENTS BY SELECTABLE COVER IMAGE ***END*** ------------------------------>
 
 
@@ -210,7 +150,6 @@ import TournamentCard from '../components/TournamentCard.vue'
 // import HomeCarousel from '../components/HomeCarousel.vue'
 import { Carousel,Slide } from 'vue3-carousel'
 
-
 export default {
 
   name: 'HomePage',
@@ -226,7 +165,6 @@ export default {
 
     const filterBy = ref('')
     const homeCarousel = ref(null)
-
 
     //NOTE If we want to randomize between an array of set background colors / images this is how we can do that
     // async getTournamentBg() {
@@ -258,11 +196,6 @@ export default {
       filterBy,
       homeCarousel,
       tournament: computed(() => AppState.activeTournament),
-    
-    myEventTickets(){
-      router.push({ name: 'EventDetails',
-                    params: {eventId: props.myTicket.event.id}})
-    },
       carouselTournaments: computed(()=>AppState.tournaments),
       tournaments: computed(() => {
         if (filterBy.value == '') {
