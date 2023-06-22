52d5ea3bcebb46db94ea2691bfd1cb8ec8397583
Chantha Kammer <cadetsa219@gmail.com>
2023-06-22 09:55:15 -0600

    Update CommentCard.vue

--- Capstone.client/src/components/CommentCard.vue
+++ Capstone.client/src/components/CommentCard.vue
@@ -21,7 +21,7 @@
         }) }} 
     </span>
   </div>
-  <div class="col-md-auto bg-light rounded-3">
+  <div class="col-md-12 bg-light rounded-3">
     <p class="text-dark"> {{ comment.body }} </p>
     <i v-if="comment.creatorId == account.id" @click="deleteComment()" class="mdi mdi-delete pe-1 icon fs-4 float-end"></i>
   </div>    
