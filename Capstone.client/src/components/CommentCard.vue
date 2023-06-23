3967bc17102b4f6ea60bd01c7bc1211a1e441f9e
Q-Mick <quentinmick@protonmail.com>
2023-06-23 12:45:02 -0600

    mobile comments view update

--- Capstone.client/src/components/CommentCard.vue
+++ Capstone.client/src/components/CommentCard.vue
@@ -1,72 +1,27 @@
 <template>
-<div class="row bg-commentcard rounded-4 elevation-3 m-3 p-3" style=" position: relative;">
-  <div class="col-md-8 m-3 p-0">
-      <router-link :to="{name: 'Profile', params: { profileId: comment.creatorId }}">
-        <img :src="comment.creator.picture" :alt="comment.creator.name" class="img-fluid object-fit-cover comment-pfp" style="max-height: 50px; margin-left:-1rem; position: absolute; top: -25px; box-shadow: 0 0 1px 1px white">
+  <div class="row bg-commentcard rounded-4 elevation-3 m-3 p-3" style=" position: relative;">
+    <div class="col-md-8 m-3 p-0" style="">
+      <router-link :to="{ name: 'Profile', params: { profileId: comment.creatorId } }">
+        <img :src="comment.creator.picture" :alt="comment.creator.name" class="img-fluid object-fit-cover comment-pfp"
+          style="max-height: 50px; margin-left:-1rem; position: absolute; top: -25px; box-shadow: 0 0 1px 1px white">
       </router-link>
-    <span style="text-shadow: 1px 1px 1px black;color: white; margin-left: 1rem; position: absolute; top:0px; right:0px;" class="mb-1 fw-light"> 
-      <span class="text-white fw-normal" style="text-shadow: 1px 1px 1px black">{{ comment.creator.name }}</span>posted on
-    {{ comment?.createdDate }}
-    @ {{ comment?.createdTime }} 
-    </span>
-  </div>
-  <div class="col-md-12 bg-light rounded-3" style="">
-    <div>
+      <span class="text-white fw-normal mobile-posted-name" style="text-shadow: 1px 1px 1px black">{{ comment.creator.name
+      }}</span>
+      <span style="text-shadow: 1px 1px 1px black;color: white;" class="mb-1 fw-light mobile-posted-on">
+        posted on
+        {{ comment?.createdDate }}
+        @ {{ comment?.createdTime }}
+      </span>
+    </div>
+    <div class="col-md-12 bg-light rounded-3" style="">
+      <div>
 
-      <p class="text-dark pt-2 mt-0 mb-1"> {{ comment.body }} </p>
-    </div>    
-    <i v-if="comment.creatorId == account.id" @click="deleteComment()" class="mdi mdi-delete pe-1 icon fs-5 float-end"><small>remove comment</small></i>
-  </div>
-</div>
-
-
-
-  <!-- <div class="col-md-8 comment-bottom p-2 px-4 rounded" style="">
-    <div class="d-flex flex-row add-comment-section mt-1 mb-4">
-  </div>
-      <div class="commented-section mt-2 ps-4">
-      <div class="d-flex flex-row align-items-center commented-user">
-        <span class="mb-1">
-          <router-link :to="{name: 'Profile', params: { profileId: comment.creatorId }}">
-            <img :src="comment.creator.picture" :alt="comment.creator.name" class="img-fluid comment-pfp" style="max-height: 50px;">
-          </router-link>
-        </span>
-        <h5 class="me-2"> {{ comment.creator.name }} </h5>
-        <span style="color: darkblue;" class="mb-1 ms-2 date"> {{
-          new Date(comment.creator.createdAt)
-            .toLocaleDateString('en-US', {
-              year: 'numeric',
-              month: 'short',
-              day: 'numeric'
-            }) }}
-        @ {{
-          new Date(comment.creator.createdAt)
-            .toLocaleTimeString('en-US', {
-              hour: 'numeric',
-              minute: 'numeric'
-            }) }}
-        </span>
+        <p class="text-dark pt-2 mt-0 mb-1"> {{ comment.body }} </p>
       </div>
-
-      <div class="comment-text"><p class="text-dark px-3"> {{ comment.body }} </p>
-        <i v-if="comment.creatorId == account.id" @click="deleteComment()" class="mdi mdi-delete pe-1 icon fs-4 float-end"></i></div>
-      <div
-        class="reply-section">
-        <div class="d-flex flex-row align-items-center voting-icons">
-          <div>
-            <i class="mdi mdi-arrow-up fa-2x mt-3 hit-voting"></i>
-          </div>
-          <div>
-            <i class="mdi mdi-arrow-down fa-2x mb-3 hit-voting-down"></i>
-          </div>
-          <span class="text-dark ms-2">Upvotes</span><span class="dot ms-2"></span>
-          <h6 class="ms-2 mt-1 reply-text">Reply</h6>
-        </div> -->
-      <!-- </div>
+      <i v-if="comment.creatorId == account.id" @click="deleteComment()"
+        class="mdi mdi-delete pe-1 icon fs-5 float-end"><small>remove comment</small></i>
     </div>
   </div>
-</div> --> 
-  
 </template>
 
 
@@ -80,10 +35,10 @@ import { Tournament } from "../models/Tournament.js"
 
 export default {
   props: {
-    comment: { type: Comment, required: true},
-    tournament:{type: Tournament,required: true }
+    comment: { type: Comment, required: true },
+    tournament: { type: Tournament, required: true }
   },
-  setup(props){
+  setup(props) {
     return {
       // formatDateAndTime,
       // currentDate: computed(() =>{
@@ -93,7 +48,7 @@ export default {
       //   return formatDateAndTime(new Date).formattedTime
       // }),
       account: computed(() => AppState.account),
-      
+
       async deleteComment() {
         try {
           const commentId = props.comment.id
@@ -123,17 +78,16 @@ export default {
 
 
 <style lang="scss" scoped>
-
-
-
-.bg-commentcard{
+.bg-commentcard {
   background-color: #374466;
   // height: 10rem;
 }
-.comment-bottom{
+
+.comment-bottom {
   background-color: #374466
 }
-.comment-pfp{
+
+.comment-pfp {
   aspect-ratio: 1/1;
   min-width: 4rem;
   max-width: 4rem;
@@ -141,6 +95,7 @@ export default {
   max-height: 4rem;
   border-radius: 50%;
 }
+
 span {
   text-shadow: 1px 1px 2px rgba(0, 0, 0, 0);
   font-weight: 650;
@@ -148,11 +103,11 @@ span {
   padding-bottom: 1rem;
 }
 
-img{
+img {
   height: 4em;
 }
 
-.icon{
+.icon {
   cursor: pointer;
   color: red;
   text-shadow: 1px 1px 2px black;
@@ -174,32 +129,43 @@ img{
 }
 
 .comments {
-text-decoration: underline;
-text-underline-position: under;
-cursor: pointer;
-}
-
-.reply-text {
-  color: darkblue;
-}
-
-.hit-voting:hover {
-  color: blue;
-}
-
-.hit-voting {
+  text-decoration: underline;
+  text-underline-position: under;
   cursor: pointer;
-  color: dodgerblue;
 }
 
-.hit-voting-down:hover {
-  color: orange;
+// .reply-text {
+//   color: darkblue;
+// }
+
+
+.mobile-posted-name {
+  position: absolute;
+  top: 15px;
+  left: 80px;
+
 }
 
-.hit-voting-down {
-  cursor: pointer;
-  color: orange;
+.mobile-posted-on {
+  position: absolute;
+  top: 15px;
+  right: 10px;
 }
 
+@media (max-width: 768px) {
+  .mobile-posted-name {
+    display: absolute;
+    top: -10px;
+    left: 80px;
+  }
+  .mobile-posted-on {
+    display: absolute;
+    position: absolute;
+    margin-left: 1rem;
+    top: 10px;
+    right: 8px;
+    margin-right: 0;
+  }
 
+}
 </style>
